202d7382fb75d0897a7fd4efdf3f5cac63c05655
Aislin Paton <aislinpaton@gmail.com>
2020-04-25 12:45:40 -0700

    Update carbon_level_grabbin.py

--- carbon_level_grabbin.py
+++ carbon_level_grabbin.py
@@ -20,3 +20,26 @@ for resource in package.resources:
 df = pd.DataFrame(co2_data)
 # extracting today's carbon level (in ppm)
 today_carbon_level = df.loc[(len(df)-1), 1]
+
+monthstart = df.dt.is_month_start()
+monthend = df.dt.is_month_end()
+
+month1 = []
+month2 = []
+
+if monthstart.loc[(len(monthstart)-1), 1] == True:
+    if len(monthstart) > 0:
+        month2.append(today_carbon_level)
+    else:
+        month1.append(today_carbon_level)
+elif monthend.loc[(len(monthend)-1), 1] == True:
+    if len(month2) > 0:
+        month2.append(today_carbon_level)
+        # Averages/Tweet TODO
+    else:
+        month1.append(today_carbon_level)
+else:
+    if len(month2) > 0:
+        month2.append(today_carbon_level)
+    else:
+        month1.append(today_carbon_level)
