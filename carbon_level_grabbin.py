b9427d371cb8ad8c5c96205ee849ab8e08a89a0f
Aislin Paton <aislinpaton@gmail.com>
2020-04-25 16:18:44 -0700

    Update carbon_level_grabbin.py

--- carbon_level_grabbin.py
+++ carbon_level_grabbin.py
@@ -19,31 +19,57 @@ for resource in package.resources:
         co2_data = resource.read()
 
 # turning the data into a dataframe
+
 df = pd.DataFrame(co2_data)
+
 # extracting today's carbon level (in ppm)
-today_carbon_level = df.loc[(len(df)-1), 1]
 
-today_date = str(df.loc[(len(df)-1), 0])
-today_date = datetime.strptime(today_date, '%Y-%m-%d')
-s = pd.Series(pd.date_range(today_date) '''needs more parameters added''')
-s.dt.is_month_start()
-#monthstart = s.dt.is_month_start()
-#monthend = s.dt.is_month_end()
+df[2] = df[0]
+
+df[3] = df[2]
+
+for i in range(len(df[2])):
+
+    date = str(df.loc[i, 2])
+
+    date = datetime.strptime(date, '%Y-%m-%d')
+
+    s = pd.Series(pd.date_range(date, periods = 1))
 
 
-'''
+    is_start = s.dt.is_month_start
+
+    df.at[i, 2] = is_start[0]
+
+
+    is_end = s.dt.is_month_end
+
+    df.at[i, 3] = is_end[0]
+
+today_date = str(df.loc[(len(df)- 1), 0])
+today_carbon_level = df.loc[(len(df)- 1), 1]
+
+
+
+
 month1 = []
 month2 = []
 
-if monthstart.loc[(len(monthstart)-1), 1] == True:
+if df.loc[(len(df)- 1), 2] == True:
     if len(month1) > 0:
         month2.append(today_carbon_level)
     else:
         month1.append(today_carbon_level)
-elif monthend.loc[(len(monthend)-1), 1] == True:
+elif df.loc[(len(df)- 1), 3] == True:
     if len(month2) > 0:
         month2.append(today_carbon_level)
-        # Averages/Tweet TODO
+        # Tweet TODO
+        month1avg = mean(month1)
+        month2avg = mean(month2)
+        if month2 > month1:
+            # sad tweet
+        else:
+            # happy tweet
     else:
         month1.append(today_carbon_level)
 else:
@@ -51,4 +77,5 @@ else:
         month2.append(today_carbon_level)
     else:
         month1.append(today_carbon_level)
-'''
+
+
